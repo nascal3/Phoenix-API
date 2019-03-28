79e7b77719be863be10540daf712c979fd9eedeb
nascal3 <nascal3@gmail.com>
2019-03-28 12:03:38 +0300

    configured corsica cors options

--- lib/my_app_web/endpoint.ex
+++ lib/my_app_web/endpoint.ex
@@ -40,5 +40,9 @@ defmodule MyAppWeb.Endpoint do
     key: "_my_app_key",
     signing_salt: "LoguCWcW"
 
+  plug Corsica,
+       origins: "http://localhost:8080",
+       log: [rejected: :error, invalid: :warn, accepted: :debug]
+
   plug MyAppWeb.Router
 end
